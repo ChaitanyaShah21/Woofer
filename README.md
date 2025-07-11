85d34f1d6a90b179f8b3589971fe59d0abc3f048
ChaitanyaShah21 <chaitanyashahdelhi@gmail.com>
2025-07-11 21:15:47 +0530

    moved project
    moved from cs50 to public repository

---
+++ README.md
@@ -0,0 +1,46 @@
+# WOOFER
+#### Video Demo:  https://www.youtube.com/watch?v=ExM4j3fED9s
+#### Description:
+
+WOOFER is a social media (Twitter) clone WebApp. It is made using
+Flask, along with HTML,CSS (and Bootstrap), and JavaScript.
+
+It allows users to create account, login and post WOOFS (tweets,
+text-only).
+
+All data is stored in a SQL database (woofer.db) using sqlite.
+There are two tables in the database, one containing user data,
+and the other containing Woof data (content, OP, timestamp).
+
+All the validation has been done server-side to ensure that only
+correct data reaches the database.
+
+All the front end web pages have been designed using Bootstrap 5.
+If the user is not logged in, user is redirected to '/login'
+route, where he can log in or go to '/register' to register a
+new user ("Create New Account" button).
+
+Homepage or "index.html" provides access to all the woofs (tweets)
+made in the platform (latest-first) and provides a way to make a
+woof.
+
+My Woofs page displays all the woofs made by the current user
+(latest-first).
+
+Profile page displays the Name, Username, and Number of Woofs
+made by the user. It also provides a way to change the user's
+password.
+
+Log out tab clears the session and logs the user out.
+
+The inspiration of the design of this WebApp is taken from Twitter and Facebook. The name Woofer is inspired by Twitter, but the twitter birf is swapped with a dog paw. All the front-end design in this app is done using Bootstrap 5. This includes the login form, register from, page layouts, nav bar, etc. The inspiration for the colour scheme is taken from Color Hunt (www.colorhunt.co). Server side validation is implemented instead of client side verification to ensure that only allowed data is entered in the database. Passwords created have to be atleast 8 characters long, with atleast one lowercase letter ,atleast one uppercase letter, atleast one number, and atleast one symbol from from _ or @ or $.
+
+As it is a very basic social media clone with only text 'woofs', only two tables are made in the database:- users table to store user details like name, username, email, userid, etc., and woofs table to store the content of the woof, orignal poster, and timestamp of when the woof was made. Woofs are display according to this timestamp, latest first.
+
+Ideas that may be implemented at a later time:-
+-> There are extra columns availible in the users table for followers and following, like that in twitter and instagram, which records the number of users followed by and following the user. An additional table is to be made to record all these interactions.
+-> The ability to like and comment a woof can be implemented. For this the woof table has to be expanded and seperate tables for likes and comments be made.
+-> If the ability to like or comment is implemented, the problem occuring during simultaneus likes and comments (only one is recorded at a time) has to be dealt with.
+-> The ability to post photos can be implemented.
+-> Only a particular ammount of woofs should be loaded at a time and rest loaded after scrolling to reduce load on server.
+-> Email verification can be implemented while creating accounts.
---
+++ app.py
@@ -0,0 +1,335 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from cs50 import SQL
+from helpers import login_required
+import re
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///woofer.db")
+
+
+# # Make sure API key is set
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+
+# Function for validating an Email
+def check_email(email):
+    # Make a regular expression
+    # for validating an Email
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    # pass the regular expression and the string into the fullmatch() method
+    if re.fullmatch(regex, email):
+        return True
+
+    else:
+        return False
+
+# Function for validating password acc to:
+#     Minimum 8 characters.
+#     The alphabet must be between [a-z]
+#     At least one alphabet should be of Upper Case [A-Z]
+#     At least 1 number or digit between [0-9].
+#     At least 1 character from [ _ or @ or $ ].
+
+
+def check_password(password):
+    flag = 0
+    while True:
+        if len(password) <= 8:
+            flag = -1
+            break
+        elif not re.search("[a-z]", password):
+            flag = -1
+            break
+        elif not re.search("[A-Z]", password):
+            flag = -1
+            break
+        elif not re.search("[0-9]", password):
+            flag = -1
+            break
+        elif not re.search("[_@$]", password):
+            flag = -1
+            break
+        elif re.search("\s", password):
+            flag = -1
+            break
+        else:
+            flag = 0
+            return True
+
+    if flag == -1:
+        return False
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+
+
+@app.route("/", methods=["GET", "POST"])
+# homepage
+@login_required
+def index():
+    data = {
+        'woof': '',
+    }
+    all_woofs = db.execute("SELECT * FROM woofs ORDER BY timestamp DESC")
+    woof_data = []
+    for woofs in all_woofs:
+        user = db.execute("SELECT * FROM users WHERE id = ?", woofs['user_id'])[0]
+        entry = {
+            'woof': woofs['woof'],
+            'timestamp': woofs['timestamp'],
+            'username': user['username'],
+            'firstName': user['firstName'],
+            'lastName': user['lastName'],
+        }
+        woof_data.append(entry)
+    if request.method == "GET":
+        return render_template("index.html", error="", woof_data=woof_data)
+
+    elif request.method == "POST":
+        woof = request.form.get("send-woof")
+        if not woof:
+            error = "You cannot send a blank WOOF!"
+            return render_template("index.html", error=error, woof_data=woof_data)
+        else:
+
+            data['woof'] = ""
+            db.execute("INSERT INTO woofs (woof, user_id) VALUES(?, ?)",
+                       woof, session["user_id"])
+
+            all_woofs = db.execute("SELECT * FROM woofs ORDER BY timestamp DESC")
+            woof_data = []
+            for woofs in all_woofs:
+                user = db.execute("SELECT * FROM users WHERE id = ?", woofs['user_id'])[0]
+                entry = {
+                    'woof': woofs['woof'],
+                    'timestamp': woofs['timestamp'],
+                    'username': user['username'],
+                    'firstName': user['firstName'],
+                    'lastName': user['lastName'],
+                }
+                woof_data.append(entry)
+            return render_template("index.html", error="", woof_data=woof_data)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    """Log user in"""
+    if request.method == "GET":
+
+        return render_template("login.html", error="")
+
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            error = "Please enter a username and password."
+            return render_template("login.html", error=error)
+
+        else:
+            user_search = db.execute("SELECT * FROM users WHERE username = ?", username)
+            if len(user_search) != 1 or not check_password_hash(user_search[0]["hash"], password):
+                error = "Invalid username and/or password"
+                return render_template("login.html", error=error)
+            session["user_id"] = user_search[0]["id"]
+            return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/my-woofs", methods=["GET"])
+# woofs by current user
+@login_required
+def my_woofs():
+
+    all_woofs = db.execute("SELECT * FROM woofs WHERE user_id = ? ORDER BY timestamp DESC", session['user_id'])
+    user = db.execute("SELECT * FROM users WHERE id = ?", session['user_id'])[0]
+    woof_data = []
+    for woofs in all_woofs:
+
+        entry = {
+            'woof': woofs['woof'],
+            'timestamp': woofs['timestamp'],
+            'username': user['username'],
+            'firstName': user['firstName'],
+            'lastName': user['lastName'],
+        }
+        woof_data.append(entry)
+    if request.method == "GET":
+        return render_template("my_woofs.html", woof_data=woof_data)
+
+
+@app.route("/profile", methods=["GET", "POST"])
+# woofs by current user
+@login_required
+def profile():
+
+    error = ""
+    success = ""
+    user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]
+    woof_num = db.execute("SELECT COUNT(*) FROM woofs WHERE user_id = ?", session["user_id"])[0]['COUNT(*)']
+    if request.method == "GET":
+        return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+    elif request.method == "POST":
+        old_password = request.form.get("old_password")
+        new_password = request.form.get("new_password")
+
+        if not old_password or not new_password:
+            error = "Please fill both old and new passwords!"
+            return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+
+        correct_hash = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]["hash"]
+
+        if not check_password_hash(correct_hash, old_password):
+            error = "Incorrect Old Password"
+            return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+
+        elif not check_password(new_password):
+            error = "New password must contain a minimum of 8 characters, including at least 1 uppercase and lowercase" \
+                    " letter, at least 1 number and at least one symbol cd ."
+            return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+
+        if old_password == new_password:
+            error = "New password cannot be same as the old password"
+            success = ""
+            return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+        # generate new hash
+        password_hash = generate_password_hash(new_password)
+        db.execute("UPDATE users SET hash = ? WHERE id = ?", password_hash, session["user_id"])
+        error = ""
+        success = "Password Successfully updated!"
+        return render_template("profile.html", user=user, woof_num=woof_num, error=error, success=success)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session.clear()
+    if request.method == "GET":
+        register_data = {
+            'fname': '',
+            'first_name': '',
+            'lname': '',
+            'last_name': '',
+            'uname': '',
+            'username': '',
+            'uname_error': 'Required.',
+            'mail': '',
+            'email': '',
+            'email_error': 'Please enter a valid email address.',
+            'pword': '',
+            'password': '',
+        }
+        return render_template("register.html", data=register_data)
+
+    elif request.method == "POST":
+
+        first_name = request.form.get("firstName")
+        last_name = request.form.get("lastName")
+        username = request.form.get("username")
+        email = request.form.get("registerEmail")
+        password = request.form.get("password")
+
+        if not first_name or not first_name.isalpha():
+            fname = "is-invalid"
+        else:
+            fname = "is-valid"
+        if not last_name or not last_name.isalpha():
+            lname = "is-invalid"
+        else:
+            lname = "is-valid"
+
+        if not username:
+            uname = "is-invalid"
+            uname_error = "Required."
+        else:
+            user_search = db.execute("SELECT * FROM users WHERE username = ?", username)
+            if len(user_search) != 0:
+                uname = "is-invalid"
+                uname_error = "Username already taken!"
+            else:
+                uname = "is-valid"
+                uname_error = ""
+        if not email or not check_email(email):
+            mail = "is-invalid"
+            email_error = "Please enter a valid email address."
+        else:
+            email_search = db.execute("SELECT * FROM users WHERE email = ?", email)
+            if len(email_search) != 0:
+                mail = "is-invalid"
+                email_error = "Email already linked to an existing account."
+            else:
+                mail = "is-valid"
+                email_error = ""
+        if not password or not check_password(password):
+            pword = "is-invalid"
+        else:
+            pword = "is-valid"
+
+        # data to be re-entered in case invalid
+        register_data = {
+            'fname': fname,
+            'first_name': first_name,
+            'lname': lname,
+            'last_name': last_name,
+            'uname': uname,
+            'username': username,
+            'uname_error': uname_error,
+            'mail': mail,
+            'email_error': email_error,
+            'email': email,
+            'pword': pword,
+            'password': password,
+        }
+
+        if fname == 'is-invalid' or lname == 'is-invalid' or uname == 'is-invalid' or mail == 'is-invalid' or pword == 'is-invalid':
+
+            return render_template("register.html", data=register_data)
+        else:
+            password_hash = generate_password_hash(password)
+            db.execute("INSERT INTO users (firstName, lastName, username, hash, email) VALUES(?, ?, ?, ?, ?)",
+                       first_name, last_name, username, password_hash, email)
+            return redirect("/login")
+
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port=5001,
+        debug=True
+    )
